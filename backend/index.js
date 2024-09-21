f2b371c28c9445a11cb30afd82c864178546688d
shahinsah2 <shahinsah@innogenx.in>
2024-09-21 16:28:33 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const leadRoutes = require("./routes/leadRoutes");
 
 //  Middleware
 app.use(cors());
-app.use(cors());
+// app.use(cors());
 // {
 //   origin: 'http://localhost:5173' // Allow only this origin
 // }
